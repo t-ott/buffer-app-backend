ca286aae1b70d27195b7e44d2ae1efd08b186a9c
Tommy Ott <tommy.ott617@gmail.com>
2023-07-31 09:45:09 -0400

    Append message to incoming JSON

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
 use actix_web::{post, App, HttpResponse, HttpServer, Responder};
+use serde_json::Value;
 
 #[post("/buffer")]
 async fn buffer(req_body: String) -> impl Responder {
-    HttpResponse::Ok().body(format!("Buffer request received:\n{}", req_body))
+    let mut req_json: Value = serde_json::from_str(&req_body).unwrap();
+    let resp: &mut serde_json::Map<String, Value> = req_json.as_object_mut().unwrap();
+    resp.insert("message".to_string(), "Buffer request received!".into());
+    HttpResponse::Ok().body(serde_json::to_string(resp).unwrap())
 }
 
 #[actix_web::main]
